51ec8615b6cf917c85d6fae09e4b92bf483f3c4a
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-06-24 13:41:40 +1000

    Updated tests

--- src/tests/RatingsTest.js
+++ src/tests/RatingsTest.js
@@ -1,1 +1,27 @@
-import 
+import assert from 'assert';
+
+import 'chromedriver';
+import { By, Builder } from 'selenium-webdriver';
+
+import BaseTest from './BaseTest';
+
+let driver = await new Builder().forBrowser('chrome').build();
+
+describe('Ratings tests', () => {
+  beforeEach(async () => {
+    await driver.manage().setTimeouts({ implicit: 3000 }); // Wait
+    await driver.manage().window().maximize(); // Maximize
+    await driver.get('https://www.seek.com.au/companies/'); // Get webpage
+  });
+
+  afterEach(async () => {
+    await driver.quit();
+  });
+
+  describe('Get ratings', () => {
+    it('should open the ratings page', async () => {
+      await navigateToCompanyCard();
+      await driver.findElement(By.className());
+    });
+  });
+});
